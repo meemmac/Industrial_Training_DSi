a2b4fa4db849ac93b891d01b38ce53a20876e3e3
Nishat Tasnim Meem <ntasnim358@gmail.com>
2026-05-18 02:41:15 +0600

    Commit task 1

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,4 +1,29 @@
 package com.example.demo;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Main Page";
+    }
+
+    @GetMapping("/hello")
+    public String helloWorld() {
+        return """
+        <html>
+          <head>
+            <title>Hello</title>
+          </head>
+          <body style="margin:0; height:100vh; display:flex; justify-content:center; align-items:center;">
+            <div style="font-size:64px; font-weight:700;">
+              Hello, World!
+            </div>
+          </body>
+        </html>
+        """;
+    }
 }
